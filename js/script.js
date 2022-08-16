9776aed29186cec48015b9bce84ee9de279560e3
Karapetyan111 <marzpetkarapetyan0@gmail.com>
2022-08-16 11:45:37 +0300

    project first commit

---
+++ js/script.js
@@ -0,0 +1,40 @@
+'use strict';
+  // $(function(){
+//     $(window).scroll(function(){
+//         $('sec3').each(function(){
+//             var x = $(window).scrollTop() + $(window).height();
+
+//             var y = $(this).offset().top + $(this).height() / 2;
+
+//             if(x >= y){
+//                 $(this).addClass("active")
+//             }
+//         })
+//     })
+// })
+// var btn1 = document.getElementById("myid");
+
+// window.onscroll = function(){
+//     scrollfunction()
+// };
+
+// function scrollfunction(params) {
+//     if(document.body.scrollTop > 50 || document.documentElement.scrollTop(
+//         )){
+        
+//     }
+//     else{
+//         btn1.style.display = 'block'  
+//     }
+
+    
+  
+    
+// }
+// function topFunction(){
+//     document.body.scrollTop = 0;
+//     document.documentElement.scrollTop = 0;
+
+// }
+
+
